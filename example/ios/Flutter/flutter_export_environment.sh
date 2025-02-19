0177730ad4a5197282918575b08206250309921a
ddwsc <ddwsc@ddwscs-iMac-2.lan>
2025-02-19 19:43:46 +0700

    Update ios

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ddwsc/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ddwsc/Downloads/flutter_pjsip-0.0.3/example"
+export "FLUTTER_APPLICATION_PATH=/Users/ddwsc/Downloads/flutter_pjsip/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
-export "TRACK_WIDGET_CREATION=false"
-export "TREE_SHAKE_ICONS=true"
-export "PACKAGE_CONFIG=/Users/ddwsc/Downloads/flutter_pjsip-0.0.3/example/.dart_tool/package_config.json"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
